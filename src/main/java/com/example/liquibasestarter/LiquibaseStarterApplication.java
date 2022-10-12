ca597c88a3204ae80a950898bc1f03ce32e13981
alexmax198361 <107352757+alexmax198361@users.noreply.github.com>
2022-10-12 19:01:52 +0300

    init commit

---
+++ src/main/java/com/example/liquibasestarter/LiquibaseStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.liquibasestarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liquibasestarter/LiquibaseStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liquibasestarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
